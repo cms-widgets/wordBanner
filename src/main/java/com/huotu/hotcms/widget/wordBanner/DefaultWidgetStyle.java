72122a53696a54f8474a7f3ef69e15e50f985184
lihuaixin <1072931117@qq.com>
2016-08-16 15:02:44 +0800

    不提供js保存，添加编辑视图浏览测试

--- src/main/java/com/huotu/hotcms/widget/wordBanner/DefaultWidgetStyle.java
+++ src/main/java/com/huotu/hotcms/widget/wordBanner/DefaultWidgetStyle.java
@@ -51,7 +51,7 @@ public class DefaultWidgetStyle implements WidgetStyle {
 
     @Override
     public Resource previewTemplate() {
-        return new ClassPathResource("/template/wordBannerStyle1PreviewTemplate.html", getClass().getClassLoader());
+        return null;
     }
 
     @Override
